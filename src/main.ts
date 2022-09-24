e722272bfd7d64659616dd8cb2992ca671eb2864
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-09-23 21:38:29 -0600

    Get app running from script

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 #!/usr/bin/env node
+
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+import * as path from "path"
+import commander from 'commander'
+
+const program = commander;
+
+clear();
+console.log(
+  chalk.red(
+    figlet.textSync('pizza-cli', { horizontalLayout: 'full' })
+  )
+);
